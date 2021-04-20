c832e633cf7935a6763d6b98a738c7dbb399ad06
shawn-yin128 <yxyoung128@gmail.com>
2021-04-20 23:36:00 +0800

    update level5/section2.4/exercise1

---
+++ level5/section3.4/exercise2/main.cpp
@@ -0,0 +1,18 @@
+// test colon syntax
+
+#include "point.hpp"
+#include "line.hpp"
+#include "circle.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    Line l;
+
+    // The constructor accepts x- and y-coordinates is called twice.
+    // The default constructor is called once.
+	// The destructor is called twice.
+
+    return 0;
+}
