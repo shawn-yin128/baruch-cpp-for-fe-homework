b7b6c43e3297da681fcf90562cf3ae4f3483e5ec
shawn-yin128 <yxyoung128@gmail.com>
2021-04-22 15:45:33 +0800

    update level section3.4 exercise2 answer

--- level5/section3.4/exercise2/main.cpp
+++ level5/section3.4/exercise2/main.cpp
@@ -13,14 +13,18 @@ int main(void) {
     Line l(Point(1,2), Point(3, 4)); // Create line.
 
     cout<<s.ToString()<<endl; // Print shape.
+                              // ID is 16807
     cout<<p.ToString()<<endl; // Print point.
+                              // Point(10, 20)
     cout<<l.ToString()<<endl; // Print line
+                              // Line starts at Point(1, 2) and end at Point(3, 4).
 
-    cout<<"Shape ID: "<<s.ID()<<endl; // ID of the shape. 16807
+    cout<<"Shape ID: "<<s.ID()<<endl; // ID of the shape. 
+                                      // Shape ID: 16807
     cout<<"Point ID: "<<p.ID()<<endl; // ID of the point. Does this work?
-                                      // work 282475249
+                                      // Point ID: 282475249
     cout<<"Line ID: "<<l.ID()<<endl; // ID of the line. Does this work?
-                                     // work 1144108930
+                                     // Line ID: 1144108930
     
     cout << endl;
 
@@ -33,8 +37,7 @@ int main(void) {
     // Create and copy Point p to new point.
     Point p2;
     p2=p;
-    cout<<p2<<", "<<p2.ID()<<endl; // Is the ID copied if you do not call
-                                   // the base class assignment in point?
+    cout<<p2<<", "<<p2.ID()<<endl; // Is the ID copied if you do not call the base class assignment in point?
                                    // No, p2 has a different id 1457850878
                                    // I think the reason is that the assignment pointer does not rewrite the id, it only change the point's attribute
 
